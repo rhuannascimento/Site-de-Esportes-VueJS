a6921f57fc7771237c33067d3e23ab1ec9d9214c
Rhuan Nascimento Ferreria <rhuan2414@gmail.com>
2022-06-21 13:48:23 -0300

    Removendo um aparte inutlizada do código

--- src/store/store.js
+++ src/store/store.js
@@ -2,8 +2,7 @@ import {createStore} from 'vuex';
 
 const store = createStore({
     state:{
-        championship: 'Campeonato Brasileiro',
-        clubName: 'Nasc Treinamentos'
+        
     },
     getters:{
         
